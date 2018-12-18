dac385b88b7af1e0d30cca52eca04c29617444c0
Staffan Widegarn Åhlvik <staffan.tan@gmail.com>
2018-12-18 12:59:54 +0100

    Update GlitchEffect.cs
    updated to work on current version of unity

--- GlitchEffect.cs
+++ GlitchEffect.cs
@@ -10,85 +10,95 @@ to make commercial use of the work
 */
 
 using UnityEngine;
-using UnityStandardAssets.ImageEffects;
-#if UNITY_EDITOR
-using UnityEditor;
-#endif
 
-#if UNITY_EDITOR
 [ExecuteInEditMode]
-[AddComponentMenu ("Image Effects/GlitchEffect")]
-#endif
-public class GlitchEffect : ImageEffectBase {
+[AddComponentMenu("Image Effects/GlitchEffect")]
+[RequireComponent(typeof(Camera))]
+public class GlitchEffect : MonoBehaviour
+{
 	public Texture2D displacementMap;
-	float glitchup, glitchdown, flicker,
-			glitchupTime = 0.05f, glitchdownTime = 0.05f, flickerTime = 0.5f;
+	public Shader Shader;
+	[Header("Glitch Intensity")]
 
-    [Header("Glitch Intensity")]
+	[Range(0, 1)]
+	public float intensity;
 
-    [Range(0, 1)]
-    public float intensity;
-    
-    [Range(0, 1)]
-    public float flipIntensity;
+	[Range(0, 1)]
+	public float flipIntensity;
 
-    [Range(0, 1)]
-    public float colorIntensity;
-	
+	[Range(0, 1)]
+	public float colorIntensity;
+
+	private float _glitchup;
+	private float _glitchdown;
+	private float flicker;
+	private float _glitchupTime = 0.05f;
+	private float _glitchdownTime = 0.05f;
+	private float _flickerTime = 0.5f;
+	private Material _material;
+
+	void Start()
+	{
+		_material = new Material(Shader);
+	}
+
 	// Called by camera to apply image effect
-	void OnRenderImage (RenderTexture source, RenderTexture destination) {
-		
-		material.SetFloat("_Intensity", intensity);
-        material.SetFloat("_ColorIntensity", colorIntensity);
-		material.SetTexture("_DispTex", displacementMap);
-        
-        flicker += Time.deltaTime * colorIntensity;
-        if (flicker > flickerTime){
-			material.SetFloat("filterRadius", Random.Range(-3f, 3f) * colorIntensity);
-            material.SetVector("direction", Quaternion.AngleAxis(Random.Range(0, 360) * colorIntensity, Vector3.forward) * Vector4.one);
-            flicker = 0;
-			flickerTime = Random.value;
+	void OnRenderImage(RenderTexture source, RenderTexture destination)
+	{
+		_material.SetFloat("_Intensity", intensity);
+		_material.SetFloat("_ColorIntensity", colorIntensity);
+		_material.SetTexture("_DispTex", displacementMap);
+
+		flicker += Time.deltaTime * colorIntensity;
+		if (flicker > _flickerTime)
+		{
+			_material.SetFloat("filterRadius", Random.Range(-3f, 3f) * colorIntensity);
+			_material.SetVector("direction", Quaternion.AngleAxis(Random.Range(0, 360) * colorIntensity, Vector3.forward) * Vector4.one);
+			flicker = 0;
+			_flickerTime = Random.value;
 		}
 
-        if (colorIntensity == 0)
-            material.SetFloat("filterRadius", 0);
-        
-        glitchup += Time.deltaTime * flipIntensity;
-        if (glitchup > glitchupTime){
-			if(Random.value < 0.1f * flipIntensity)
-				material.SetFloat("flip_up", Random.Range(0, 1f) * flipIntensity);
+		if (colorIntensity == 0)
+			_material.SetFloat("filterRadius", 0);
+
+		_glitchup += Time.deltaTime * flipIntensity;
+		if (_glitchup > _glitchupTime)
+		{
+			if (Random.value < 0.1f * flipIntensity)
+				_material.SetFloat("flip_up", Random.Range(0, 1f) * flipIntensity);
 			else
-				material.SetFloat("flip_up", 0);
-			
-			glitchup = 0;
-			glitchupTime = Random.value/10f;
+				_material.SetFloat("flip_up", 0);
+
+			_glitchup = 0;
+			_glitchupTime = Random.value / 10f;
 		}
 
-        if (flipIntensity == 0)
-            material.SetFloat("flip_up", 0);
+		if (flipIntensity == 0)
+			_material.SetFloat("flip_up", 0);
 
-
-        glitchdown += Time.deltaTime * flipIntensity;
-        if (glitchdown > glitchdownTime){
-			if(Random.value < 0.1f * flipIntensity)
-				material.SetFloat("flip_down", 1 - Random.Range(0, 1f) * flipIntensity);
+		_glitchdown += Time.deltaTime * flipIntensity;
+		if (_glitchdown > _glitchdownTime)
+		{
+			if (Random.value < 0.1f * flipIntensity)
+				_material.SetFloat("flip_down", 1 - Random.Range(0, 1f) * flipIntensity);
 			else
-				material.SetFloat("flip_down", 1);
-			
-			glitchdown = 0;
-			glitchdownTime = Random.value/10f;
+				_material.SetFloat("flip_down", 1);
+
+			_glitchdown = 0;
+			_glitchdownTime = Random.value / 10f;
 		}
 
-        if (flipIntensity == 0)
-            material.SetFloat("flip_down", 1);
+		if (flipIntensity == 0)
+			_material.SetFloat("flip_down", 1);
 
-        if (Random.value < 0.05 * intensity){
-			material.SetFloat("displace", Random.value * intensity);
-			material.SetFloat("scale", 1 - Random.value * intensity);
-        }
-        else
-			material.SetFloat("displace", 0);
-		
-		Graphics.Blit (source, destination, material);
+		if (Random.value < 0.05 * intensity)
+		{
+			_material.SetFloat("displace", Random.value * intensity);
+			_material.SetFloat("scale", 1 - Random.value * intensity);
+		}
+		else
+			_material.SetFloat("displace", 0);
+
+		Graphics.Blit(source, destination, _material);
 	}
 }
